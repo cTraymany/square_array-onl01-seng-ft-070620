6862f9e52846719172885cb64c08cda72ce62bbe
Chindalath Traymany <chindalatht@gmail.com>
2020-07-08 02:57:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,7 @@
-=begin
 def square_array(array)
   new_array = []
   array.each do |index|
     new_array << index ** 2
   end
   new_array
-end
-=end
-
-def square_array(array)
-  new_numbers = []
-  array.each do |numbers|
-    return new_numbers = numbers**2
-  end
 end
